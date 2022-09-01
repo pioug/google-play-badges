36fce295f8f22affa30e9d3d1b89215a70ce8575
Gilles Piou <gilles.piou@gmail.com>
2022-09-02 01:46:18 +0800

    Push initial commit

---
+++ svgo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  floatPrecision: 2,
+  multipass: true,
+  js2svg: {
+    indent: 2, // string with spaces or number of spaces. 4 by default
+    pretty: true, // boolean, false by default
+  },
+};
